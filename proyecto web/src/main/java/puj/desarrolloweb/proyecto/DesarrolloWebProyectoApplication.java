d696627e075c2c2919c8dd0fef355682adabac85
Juan Sebastián Mondragón <the.juan.mondra@gmail.com>
2024-08-15 16:10:08 -0500

    Spring template
    Se carga el template de Spring junto con las dependencias necesarias
    - Agosto 15, 2024

---
+++ proyecto web/src/main/java/puj/desarrolloweb/proyecto/DesarrolloWebProyectoApplication.java
@@ -0,0 +1,13 @@
+package puj.desarrolloweb.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesarrolloWebProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesarrolloWebProyectoApplication.class, args);
+	}
+
+}
---
+++ proyecto web/src/test/java/puj/desarrolloweb/proyecto/DesarrolloWebProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package puj.desarrolloweb.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesarrolloWebProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
