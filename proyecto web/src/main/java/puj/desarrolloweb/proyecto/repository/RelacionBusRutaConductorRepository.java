b9a28d65b51450479e19307633c56bd8fa5f3230
Juan Sebastián Mondragón <the.juan.mondra@gmail.com>
2024-08-20 16:30:25 -0500

    Update RelacionBusRutaConductorRepository.java

---
+++ proyecto web/src/main/java/puj/desarrolloweb/proyecto/repository/RelacionBusRutaConductorRepository.java
@@ -0,0 +1,9 @@
+package puj.desarrolloweb.proyecto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import puj.desarrolloweb.proyecto.model.RelacionBusRutaConductor;
+
+public interface RelacionBusRutaConductorRepository extends JpaRepository<RelacionBusRutaConductor, Long> {
+    
+}
