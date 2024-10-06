37db60bda5847046af4d6da60ce24ccf0e6c56c5
Juan Sebastián Mondragón <the.juan.mondra@gmail.com>
2024-10-06 12:50:55 -0500

    Corrección comunicación app Angular

--- proyectoweb/src/main/java/puj/desarrolloweb/proyecto/controller/ConductorController.java
+++ proyectoweb/src/main/java/puj/desarrolloweb/proyecto/controller/ConductorController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import puj.desarrolloweb.proyecto.service.ConductorService;
 
 @RestController
 @RequestMapping("/api/conductores")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ConductorController {
 
     @Autowired
