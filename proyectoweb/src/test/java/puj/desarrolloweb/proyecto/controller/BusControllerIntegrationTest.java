c925239a2c2b386d92c4e1e3f9ced4bad673aa44
Juan Sebastián Mondragón <the.juan.mondra@gmail.com>
2024-10-15 17:25:39 -0500

    Base pruebas controladores

---
+++ proyectoweb/src/test/java/puj/desarrolloweb/proyecto/controller/BusControllerIntegrationTest.java
@@ -0,0 +1,41 @@
+package puj.desarrolloweb.proyecto.controller;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//import java.math.BigDecimal;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import puj.desarrolloweb.proyecto.repository.BusRepository;;;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("integration-testing")
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+public class BusControllerIntegrationTest {
+    
+    private final String SERVER_URL;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private BusRepository busRepository;
+
+    public BusControllerIntegrationTest(@Value("${server.port}") int serverPort) {
+        SERVER_URL = "http://localhost:" + serverPort;
+    }
+
+    @BeforeEach
+    void init(){
+        
+    }
+
+}
