f2627bf5fbc749c48537e2b03f1a6b3c34da9299
Harry Esteban Sanchez <124279845+Harrysg1@users.noreply.github.com>
2024-11-08 01:04:32 -0500

    prueba final funciona

--- proyectoweb/src/test/java/puj/desarrolloweb/proyecto/e2e/CasoUso.java
+++ proyectoweb/src/test/java/puj/desarrolloweb/proyecto/e2e/CasoUso.java
@@ -2,10 +2,12 @@ package puj.desarrolloweb.proyecto.e2e;
 
 import java.time.Duration;
 
+import org.openqa.selenium.support.ui.Select;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -46,6 +48,7 @@ public class CasoUso {
 
     @Test
     public void test() {
+        //Entra a login
         driver.get(BASE_URL);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("txtEmail")));
         WebElement correoLabel = driver.findElement(By.id("txtEmail"));
@@ -66,14 +69,17 @@ public class CasoUso {
         WebElement btnCreate = driver.findElement(By.id("btnCreatebus"));
         btnCreate.click();
         
+        //Va al driver management
         driver.get("http://localhost:4200/conductores");
 
-
+        
+        //Crea conductor
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btnCreateConductor")));
         WebElement btnCreateConductor = driver.findElement(By.id("btnCreateConductor"));
         btnCreateConductor.click();
 
 
+        //Agraga los datos del nuevo conductor
         WebElement nombre = driver.findElement(By.id("nombre"));
         nombre.sendKeys("Harry");
         WebElement cedula = driver.findElement(By.id("cedula"));
@@ -86,6 +92,7 @@ public class CasoUso {
         WebElement btnGuardar = driver.findElement(By.id("btnGuardar"));
         btnGuardar.click();
 
+        //Va a la ultima columna, para ver si se creo el conductor con los datos y crea los assertion si esta bien
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//html//body//app-root//div//div//app-conductor//div//app-list//table//tbody//tr[101]//td[2]")));
         WebElement  nombreassert =driver.findElement(By.xpath("//html//body//app-root//div//div//app-conductor//div//app-list//table//tbody//tr[101]//td[2]"));
         Assertions.assertThat(nombreassert.getText()).isEqualTo("Harry");
@@ -94,15 +101,71 @@ public class CasoUso {
         WebElement  direccionassert =driver.findElement(By.xpath("//html//body//app-root//div//div//app-conductor//div//app-list//table//tbody//tr[101]//td[4]"));
         Assertions.assertThat(direccionassert.getText()).isEqualTo("Calle 123");
 
+        //Va al boton de asignar bus
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//html//body//app-root//div//div//app-conductor//div//app-list//table//tbody//tr[101]/td[5]//button")));
         WebElement btnAsignar = driver.findElement(By.xpath("//html//body//app-root//div//div//app-conductor//div//app-list//table//tbody//tr[101]/td[5]//button"));
         btnAsignar.click();
         
+        //Selecciona el bus 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("bus-select")));
+        WebElement busSelectElement = driver.findElement(By.id("bus-select"));
+        Select busSelect = new Select(busSelectElement);
+        busSelect.selectByVisibleText("ABC123 (BRT)");
 
+        //Presiona el boton del asingar el bus
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btnSumit")));
+        WebElement btnSumit = driver.findElement(By.id("btnSumit"));
+        btnSumit.click();
         
+        //para darle ok en la pantalla emergente
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        assert alertText.equals("Bus assigned successfully!");
+        alert.accept();
+        
+        //Selecciona los dias 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//html//body//app-root//div//div//app-asignar-bus//div//div//div//div//form//div//button[3]")));
+        WebElement btnMartes = driver.findElement(By.xpath("//html//body//app-root//div//div//app-asignar-bus//div//div//div//div//form//div//button[3]"));
+        btnMartes.click();
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//html//body//app-root//div//div//app-asignar-bus//div//div//div//div//form//div//button[4]")));
+        WebElement btnMiercoles = driver.findElement(By.xpath("//html//body//app-root//div//div//app-asignar-bus//div//div//div//div//form//div//button[4]"));
+        btnMiercoles.click();
+
+
+        //Guarda los dias
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("btnSaveDays")));
+        WebElement btnSaveDays = driver.findElement(By.id("btnSaveDays"));
+        btnSaveDays.click();
         
-        
+        //Le da ok a la pantalla emergente, lo que significa que si se ectualizaron los dias 
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert2 = driver.switchTo().alert();
+        String alertText2 = alert2.getText();
+        assert alertText2.equals("Days updated successfully!");
+        alert2.accept();
+
+
+        // Va al driver management
+        driver.get("http://localhost:4200/conductores");
+
+        // Va a la última fila, donde se creó el registro
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//html//body//app-root//div//div//app-conductor//div//app-list//table//tbody//tr[last()]")));
+        WebElement lastRow = driver.findElement(By.xpath("//html//body//app-root//div//div//app-conductor//div//app-list//table//tbody//tr[last()]"));
+
+        // Lo borra
+         //Va al boton de asignar bus
+         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//html//body//app-root//div//div//app-conductor//div//app-list//table//tbody//tr[101]/td[6]//button")));
+         WebElement btnborrar = driver.findElement(By.xpath("//html//body//app-root//div//div//app-conductor//div//app-list//table//tbody//tr[101]/td[6]//button[2]"));
+         btnborrar.click();
+       
+
+        // Envía asserts de que lo borró correctamente
+        wait.until(ExpectedConditions.invisibilityOf(lastRow));
+        Assertions.assertThat(driver.findElements(By.xpath("//html//body//app-root//div//div//app-conductor//div//app-list//table//tbody//tr[101]")).isEmpty()).isTrue();
+                
+  
 
     }
 }
